91ac7005c8b62a6c6e624bb07b6604f677b71375
neyney <neyney10@live.com>
2018-11-13 18:16:36 +0200

    clean-up imports in Main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,4 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Paint;
-import java.awt.Shape;
-import java.awt.Stroke;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.text.Format;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
 import myMath.Polynom;
-import myMath.StdDraw;
-
 
 public class Main {
 
